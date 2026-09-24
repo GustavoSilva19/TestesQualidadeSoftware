2f1a990a803b509b605e2cd93c5ec1f80f9860a1
GustavoSilva19 <163468293+GustavoSilva19@users.noreply.github.com>
2026-09-24 12:27:10 -0300

    Update test_api_client.py

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -2,14 +2,14 @@ import pytest
 from unittest.mock import patch
 from src.api_cliente import buscar_dados
 
-@patch("src.api_client.requests.get")
+@patch("src.api_cliente.requests.get")
 def test_buscar_dados_sucesso(mock_get):
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {"msg": "ok"}
     resultado = buscar_dados("http://fakeurl.com")
     assert resultado == {"msg": "ok"}
 
-@patch("src.api_client.requests.get")
+@patch("src.api_cliente.requests.get")
 def test_buscar_dados_falha(mock_get):
     mock_get.return_value.status_code = 404
     with pytest.raises(ConnectionError):
